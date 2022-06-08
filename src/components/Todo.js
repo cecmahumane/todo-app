6230c6331da0646e20751a626cfe7e3b5ef6b483
Cecil Mahumane <cecmahumane@gmail.com>
2022-06-08 09:56:40 -0400

    CSS flex and on hover feature added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -21,17 +21,19 @@ export default class Todo extends Component {
     render() {
         // console.log(this.state)
         return (
-            <div>
-               <input type='checkbox' onChange={() => this.props.markCompleted(this.props.id)}/>
-               { this.props.toEdit ? 
-                <form onSubmit={this.onSubmit}>
-                    <input type='text' name='title' placeholder={this.props.title} onChange={this.onChange} value={this.state.title}/>
-                    <input type='submit' value='Save Edit' className="submit-btn" />
-                </form>           
-                 : <p>{this.props.todo}</p>}
-               { this.props.toEdit === false && <button onClick={() => this.props.editTodo(this.props.id, this.state.title)}>Edit</button>}
-               <button onClick={() => this.props.deleteTodo(this.props.id)}>Delete</button>
-               <hr/>
+            <div className="whole-todo-element">
+               <div className="todo-element">
+                <input className="checkbox-btn" type='checkbox' onChange={() => this.props.markCompleted(this.props.id)}/>
+                { this.props.toEdit ? 
+                    <form onSubmit={this.onSubmit}>
+                        <input type='text' name='title' placeholder={this.props.title} onChange={this.onChange} value={this.state.title}/>
+                        <input type='submit' value='Save Edit' className="submit-btn" />
+                    </form>           
+                    : <p>{this.props.todo}</p>}
+                { this.props.toEdit === false && <button className="edit-btn" onClick={() => this.props.editTodo(this.props.id, this.state.title)}>Edit</button>}
+                <button className="delete-btn" onClick={() => this.props.deleteTodo(this.props.id)}>Delete</button>
+               </div> 
+               <hr className="divider"/>
             </div>
         )
     }
